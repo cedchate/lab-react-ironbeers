f972bf1160a4adf74a5185f40819b47cadf5ce74
Cedric Chatellier <cedricchatellier95.com>
2023-03-03 18:18:30 +0100

    added bonus

--- src/pages/Beers.js
+++ src/pages/Beers.js
@@ -4,19 +4,42 @@ import BeerLi from "../components/BeerLi";
 
 const Beers = () => {
   const [allBeers, setAllBeers] = useState([]);
+  const [query, setQuery] = useState("");
   useEffect(() => {
     fetch("https://ih-beers-api2.herokuapp.com/beers")
       .then((res) => res.json())
       .then((res) => setAllBeers(res))
       .catch((error) => console.log(error));
   }, []);
-  if (allBeers === []) {
+  let beerToDisplay = allBeers;
+  if (query !== "") {
+    beerToDisplay = allBeers.filter((beer) => {
+      return beer.name.toLowerCase().includes(query.toLowerCase());
+    });
+  }
+  console.log(beerToDisplay.length);
+  if (beerToDisplay === []) {
     return <></>;
   }
   // console.log(allBeers)
   return (
     <div>
-      {allBeers.map((beer) => {
+      <input
+        style={{
+          width: "80vw",
+          borderRadius: "50px",
+          height: "5vh",
+          borderColor: "#d5d3d3",
+          borderStyle: "solid",
+          paddingLeft: "5vw",
+          marginTop: "2vh",
+        }}
+        type="text"
+        value={query}
+        name="search"
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      {beerToDisplay.map((beer) => {
         return <BeerLi key={beer._id} beer={beer} />;
       })}
     </div>
